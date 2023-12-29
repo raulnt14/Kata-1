91d8be3c1da20e3f056965acea59fa2c43f4503b
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2023-12-29 13:26:07 +0000

    Atributes, constructor and getters and setters created for Person class

--- src/Software/Ulpgc/Kata1/Person.java
+++ src/Software/Ulpgc/Kata1/Person.java
@@ -1,4 +1,37 @@
 package Software.Ulpgc.Kata1;
 
 public class Person {
+    private String name;
+    private String surname;
+    private String birthday;
+
+    public Person(String name, String surname, String birthday) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 }
